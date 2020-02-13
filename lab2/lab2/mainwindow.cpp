f47a95432b835227dbaee0155b76242103dd93f8
MrPipiKado <ammamm23102310@gmail.com>
2020-02-13 23:48:48 +0200

    lab2

--- lab2/lab2/mainwindow.cpp
+++ lab2/lab2/mainwindow.cpp
@@ -1,7 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +12,4 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
